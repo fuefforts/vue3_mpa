9e398a5437e6750d7116d533fafff84885dc0c8d
fuqinghui <fuefforts@163.com>
2023-07-13 16:02:47 +0800

    vue3单页面

---
+++ src/Project/pageone/App.vue
@@ -0,0 +1,17 @@
+<template>子页面1</template>
+
+<script setup>
+const test = ref('Composition API 自动引入')
+console.log(test.value)
+</script>
+<style lang="scss">
+#app {
+  font-family: PingFangSC-Regular;
+  font-size: 4vw;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/Project/pagetwo/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script setup></script>
+<style lang="scss">
+#app {
+  font-family: PingFangSC-Regular;
+  font-size: 3.733vw;
+}
+</style>
